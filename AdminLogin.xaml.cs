6bd2291276097f54ab5d1076ddb959dd903f3774
Parsa M H <108180586+TheHanza@users.noreply.github.com>
2022-06-29 15:22:18 +0430

    Add files via upload

---
+++ AdminLogin.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _FinalProj
+{
+    public partial class AdminLogin : Window
+    {
+        public AdminLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+            WPF.MainWindow newWindow = new WPF.MainWindow();
+            newWindow.Show();
+            Close();
+        }
+    }
+}
---
+++ UserLogin.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _FinalProj
+{
+    public partial class UserLogin : Window
+    {
+        public UserLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+            WPF.MainWindow newWindow = new WPF.MainWindow();
+            newWindow.Show();
+            Close();
+        }
+
+        private void SignUp_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Login_Tabs.SelectedIndex = 1;
+        }
+    }
+}
